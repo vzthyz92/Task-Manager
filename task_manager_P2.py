98e6323b621879f32dd2ab77c206077bd8e6df9f
Thys van Zyl <vzthyz@gmail.com>
2026-06-11 06:03:58 +0200

    Add files via upload

---
+++ task_manager_P2.py
@@ -0,0 +1,261 @@
+# ===== Importing external modules ===========
+'''This is the section where you will import modules'''
+
+#to import the current date and set format
+from datetime import datetime
+
+#modify local file path here (if necessary)
+FILE_NAME1 = "user.txt"
+FILE_NAME2 = "tasks.txt"
+
+# ==== Login Section ====
+'''Implement the following functionality
+     Here you will write code that will allow a user to login.
+   - You can use a list or dictionary to store a list of usernames and passwords from the file.
+   - Use a while loop to validate your user name and password.'''
+
+#open the file user.txt and read the data from this file into a dictionary, 
+#then it will validate the username and password entered. 
+#only admin is allowed to register new users, view completed tasks and delete tasks
+
+#empty dictionary for username and password tuples
+login_dict = {}
+
+#try open file and store data tuple in dictionary if the file exists
+try:
+    #read data from file and clean data
+    with open(FILE_NAME1, "r", encoding="utf-8") as file:
+
+        for line in file:
+            username, password = line.strip().split(", ")
+            login_dict[username] = password
+               
+#incase if file not found, then display msg to user
+except FileNotFoundError:
+    print("Error! The file user.txt was not found")
+    exit()
+
+
+while True:
+    #prompt for username and password
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+
+    #check if the username provided exists and if the p/w provided matches the p/w on record
+    if username in login_dict and login_dict[username] == password:
+        print("Successfully logged in!")
+        break
+
+    else:
+        #incase the username DNE or p/w do not match
+        print("Invalid username or password entered. Please try again.")
+
+
+while True:
+    # Present the menu to the user and make sure that the user input is converted to lower case.
+    #I took the liberty of modifying the standard menu to be a bit more aesthetic than in the template
+
+    #menu for admin only
+    if username == "admin":
+        print("\n**** Main Menu ****")
+        print("1. Enter \"r\" to register a new user")
+        print("2. Enter \"a\" to add a new task")
+        print("3. Enter \"va\" to view all tasks")
+        print("4. Enter \"vm\" to view my tasks")
+        print("5. Enter \"vc\" to view completed tasks")
+        print("6. Enter \"del\" to delete tasks")
+        print("7. Enter \"e\" to exit")
+
+    #menu for everyone else
+    else:
+        print("\n**** Main Menu ****")
+        print("1. Enter \"a\" to add a new task")
+        print("2. Enter \"va\" to view all tasks")
+        print("3. Enter \"vm\" to view my tasks")
+        print("4. Enter \"e\" to exit")
+    
+    #convert user selection to lowercase to avoid errors
+    menu = input("Enter selection: ").lower()
+
+    #This option, previously available to all users should is now only available for admin
+    if menu == 'r' and username == "admin":
+        '''This code block will add a new user to the user.txt file
+        - Check if the new p/w and old p/w are the same,then add them to the user.txt file or present error message'''
+        
+        new_user = input("Enter an username for the new user: ")
+
+        #if the username already exists in txt file
+        if new_user in login_dict:
+            print("The username you have entered already exists!")
+            continue
+
+        #prompt for new p/w and confirmation p/w
+        new_pw = input("Enter the password for the new user: ")
+        confirm_pw = input("Please confirm your password: ")
+
+        #check if the passwords match, else display error to user
+        if new_pw != confirm_pw:
+            print("The passwords you have entered do not match!")
+            continue
+        
+        #write to file in tuple format        
+        with open(FILE_NAME1, "a", encoding="utf-8") as file:
+            file.write(f"\n{new_user}, {new_pw}")
+        
+        #create username and p/w pair
+        login_dict[new_user] = new_pw
+        print("\nNew user has been created successfully.")  
+
+
+    #if user selects to add a new task
+    elif menu == 'a':
+        '''This code block will allow a user to add a new task to task.txt file
+              -Then, get the current date, and ad the data to the file task.txt
+        '''
+        #get data for tasks using input and get current date and time using function
+        task_user = input("Please enter the username associated with this task: ")
+        task_title = input("Please enter the title of this task: ")
+        task_desc = input("Please enter a short description of the task: ")
+        due_date = input("Please enter the task deadline (e.g. 23 Feb 2026)")
+        date_assigned = datetime.today().strftime("%d %b %Y")
+        completed = "No"
+
+        #append new task to file
+        with open(FILE_NAME2, "a", encoding="utf-8") as file:
+            file.write(f"\n{task_user}, {task_title}, {task_desc}, {date_assigned}, {due_date}, {completed}")
+            
+        print("New task successfully created. ")
+
+
+    #if user selects to view all tasks
+    elif menu == 'va':
+        '''This code block will read the task from task.txt file and print to the console in the format of Output 2 presented in the PDF
+        '''
+
+        #read contents from file and clean data
+        with open(FILE_NAME2, "r", encoding="utf-8") as file:
+            for line in file:
+                contents = line.strip().split(", ")
+
+                #display task data in the format provided in template
+                print("\n-------------------------------")
+                print(f"Assigned to:\t {contents[0]}")
+                print(f"Task:\t\t {contents[1]}")
+                print(f"Description:\t {contents[2]}")
+                print(f"Date assigned:\t {contents[3]}")
+                print(f"Due date:\t {contents[4]}")
+                print(f"Completed:\t {contents[5]}")
+                print("-------------------------------")
+
+
+    #if selects to view only their tasks
+    elif menu == 'vm':
+        '''This code block will read the task from task.txt file and
+         print to the console in the format of Output 2 presented in the PDF
+         '''
+        
+        # read contents from file and clean data
+        with open(FILE_NAME2, "r", encoding="utf-8") as file:
+            for line in file:
+                contents = line.strip().split(", ")
+        
+                #display task data in desired format if it matches with username
+                if contents[0] == username:       
+                    print("\n-------------------------------")
+                    print(f"Assigned to:\t {contents[0]}")
+                    print(f"Task:\t\t {contents[1]}")
+                    print(f"Description:\t {contents[2]}")
+                    print(f"Date assigned:\t {contents[3]}")
+                    print(f"Due date:\t {contents[4]}")
+                    print(f"Completed:\t {contents[5]}")
+                    print("-------------------------------")
+        
+
+    #an option for admin to view only completed tasks
+    elif menu == 'vc' and username == "admin":
+
+        #read contents from file and clean data
+        with open(FILE_NAME2, "r", encoding="utf-8") as file:
+            for line in file:
+                contents = line.strip().split(", ")
+        
+                #display task data in desired format if marked as complete
+                if contents[5].strip().lower() == "yes":       
+                    print("\n-------------------------------")
+                    print(f"Assigned to:\t {contents[0]}")
+                    print(f"Task:\t\t {contents[1]}")
+                    print(f"Description:\t {contents[2]}")
+                    print(f"Date assigned:\t {contents[3]}")
+                    print(f"Due date:\t {contents[4]}")
+                    print(f"Completed:\t {contents[5]}")
+                    print("-------------------------------")
+
+                else:
+                    print("Unfortunately, no tasks have been completed!")
+        
+
+    #an option for admin to delete tasks 
+    elif menu == 'del' and username == "admin":
+        
+        tasks = []
+        
+        #read contents of file into a list
+        with open(FILE_NAME2, "r", encoding="utf-8") as file:
+            tasks = file.readlines()
+        
+        #incase no tasks exist in file
+        if not tasks:
+            print("Admin, there are no tasks available to delete.")
+            continue
+
+        print("\nSelect a task to delete:\n")
+
+        #numbering the tasks for easy and accurtate selection
+        for i, line in enumerate(tasks, start=1):
+            contents = line.strip().split(", ")
+
+        #summary view for identification
+            print(f"""
+                Task {i}
+                Assigned to: {contents[0]}
+                Task title: {contents[1]}
+                Date assigned: {contents[3]}
+                Due date: {contents[4]}
+                Completed: {contents[5]}
+                """)
+
+        try:
+            #prompt admin to select task to delete or to cancel
+            index = int(input("Enter task index to delete (enter -1 to cancel): "))
+
+            #cancel operation
+            if index == -1:
+                print("\nTask deletion cancelled succesfully.")
+                continue
+
+            #remove task            
+            if 1 <= index <= len(tasks):
+                deleted_task = tasks.pop(index - 1)
+
+                # Overwrite the text file without newly deleted task
+                with open(FILE_NAME2, "w", encoding="utf-8") as file:
+                    file.writelines(tasks)
+
+                print("The selected task has been successfully deleted.")
+                print("Removed task:", deleted_task)
+
+            else:
+                print("Admin, you entered an invalid task index number.")
+
+        #incase of bad input
+        except ValueError:
+            print("Please enter a valid task index number.")
+
+
+    #incase any user decides to exit
+    elif menu == 'e':
+        print('Goodbye!!!')
+        exit()
+
+    else:
+        print("You have entered an invalid input. Please try again")
